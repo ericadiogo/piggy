7ec0c2d53425ae67c6a7f9dee16c005bcaba46b0
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-08-19 23:35:45 -0400

    PIGGY-0004: Added launcher screen file and all screen files

---
+++ lib/screens/launcher_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class LauncherScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _LauncherScreenState();
+}
+
+class _LauncherScreenState extends State<LauncherScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFA3B2),
+      body: Container(
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/piggy_coins_glasses.png',width: 200),
+            SizedBox(height: 40,),
+            Text('PIGGY',
+                style: TextStyle(
+                  fontSize: 80,
+                  fontFamily: 'Lilita One',
+                )
+            ),
+            Text('launcher',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 40,),
+            ElevatedButton(onPressed: (){},
+                child: Text('Log in',
+                  style: TextStyle(
+                    color: Color(Colors.white),
+                  ),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:piggy/screens/launcher_screen.dart';
 
 class SplashScreen extends StatefulWidget{
   @override
   State<StatefulWidget> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>{
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(Duration(seconds: 4),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => LauncherScreen(),
+      ));
+    });
+  }
+
+  @override
+  void dispose(){
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
