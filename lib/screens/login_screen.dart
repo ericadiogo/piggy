0a080773ad536d0c227ad12d4ed1c8bdae630b29
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-08-20 16:28:52 -0400

    PIGGY-0005: Added register design, register button and routes.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:piggy/screens/register_screen.dart';
+
+class LoginScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFA3B2),
+      body: Container(
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/piggy_coins_glasses.png',width: 200),
+            SizedBox(height: 40,),
+            Text('PIGGY',
+                style: TextStyle(
+                  fontSize: 80,
+                  fontFamily: 'Lilita One',
+                )
+            ),
+            Text('Your Financial Planner',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 80,),
+            ElevatedButton(onPressed: (){},
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.black),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(10),),
+                fixedSize: MaterialStateProperty.all<Size>(Size(200.0, 60.0),),
+              ),
+              child: Text('Log In',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            new InkWell(
+              child: new Text('Register Here', style: TextStyle(
+                fontSize: 20,
+              ),
+              ),
+              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen(),),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+
 class RegisterScreen extends StatefulWidget{
   @override
   State<StatefulWidget> createState() => _RegisterScreenState();
@@ -19,25 +21,71 @@ class _RegisterScreenState extends State<RegisterScreen>{
             children: [
         Text('Register',
             style: TextStyle(
-              fontSize: 60,
+              fontSize: 50,
               fontFamily: 'Lilita One',
             )
         ),
-        Text('Your Financial Planner',
-          style: TextStyle(fontSize: 20),
+        SizedBox(height: 20,),
+        Container(
+          padding: EdgeInsets.fromLTRB(30,5,30,5),
+          child:
+            TextField(decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    width: 1.0,
+                  ),
+                ),
+                hintText: 'Email',
+              ),
+            ),
         ),
-        SizedBox(height: 80,),
-        ElevatedButton(onPressed: (){},
+        SizedBox(height: 10,),
+        Container(
+          padding: EdgeInsets.fromLTRB(30,5,30,5),
+          child:
+            TextField(decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    width: 1.0,
+                  ),
+                ),
+                hintText: 'Password',
+              ),
+            ),
+        ),
+        SizedBox(height: 10,),
+        Container(
+          padding: EdgeInsets.fromLTRB(30,5,30,5),
+          child:
+            TextField(decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    width: 1.0,
+                  ),
+                ),
+                hintText: 'Re-enter Password',
+              ),
+            ),
+        ),
+        SizedBox(height: 60,),
+        ElevatedButton(onPressed: (){
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => LoginScreen(),),);
+        },
           style: ButtonStyle(
             backgroundColor: MaterialStatePropertyAll<Color>(Colors.black),
             shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
               ),
             ),
             padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(10),),
             fixedSize: MaterialStateProperty.all<Size>(Size(200.0, 60.0),),
           ),
-          child: Text('Log In',
+          child: Text('Register',
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
@@ -46,15 +94,24 @@ class _RegisterScreenState extends State<RegisterScreen>{
           ),
         ),
         SizedBox(height: 15,),
-        new InkWell(
-          child: new Text('Register Here', style: TextStyle(
-            fontSize: 20,
-          ),
-          ),
-          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()),
-          ),
-        ),
-            ],
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('I have an account. ', style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            new InkWell(
+              child: new Text('Log in.', style: TextStyle(
+                  fontSize: 16,
+                color: Colors.blue,
+                ),
+              ),
+              onTap: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LoginScreen(),),
+              ),
+            ),],
+          ),],
         ),
       ),
     );
