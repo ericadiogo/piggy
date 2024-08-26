0722ab8e8424734a3d3adf60df3e67b6c403e0ef
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-08-25 21:58:36 -0400

    User insertion on the Database completed

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:piggy/services/firebase_auth_methods.dart';
 
 import 'login_screen.dart';
 
-class RegisterScreen extends StatefulWidget{
+class RegisterScreen extends StatefulWidget {
   static String routeName = '/signup-email-password';
-  const RegisterScreen({Key? key}) : super(key : key);
+  const RegisterScreen({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _RegisterScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen>{
+class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
@@ -25,16 +27,50 @@ class _RegisterScreenState extends State<RegisterScreen>{
   }
 
   void registerUser() async {
-    FirebaseAuthMethods(FirebaseAuth.instance).signUpWithEmail(
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: emailController.text,
         password: passwordController.text,
-        context:context,);
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('You are registered!'),
-      ),
-    );
-    Navigator.push(context,
-      MaterialPageRoute(builder: (context) => LoginScreen(),),);
+      );
+
+      User? user = userCredential.user;
+      if (user != null) {
+        await saveUserToDatabase(user);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('You are registered!'),
+          ),
+        );
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginScreen(),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Registration failed: $e'),
+        ),
+      );
+    }
+  }
+
+  Future<void> saveUserToDatabase(User user) async {
+    final DatabaseReference dbRef = FirebaseDatabase.instance.ref().child('users/${user.uid}');
+
+    await dbRef.set({
+      'uuid': user.uid,
+      'name': nameController.text,
+      'email': emailController.text,
+      'password': passwordController.text,
+      'piggys': [],
+      'income': 0.0,
+      'expenses': 0.0,
+    });
   }
 
   @override
@@ -45,120 +81,133 @@ class _RegisterScreenState extends State<RegisterScreen>{
         width: double.infinity,
         alignment: Alignment.center,
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-        Text('Register',
-            style: TextStyle(
-              fontSize: 50,
-              fontFamily: 'Lilita One',
-            )
-        ),
-        SizedBox(height: 40,),
-        Container(
-          padding: EdgeInsets.fromLTRB(30,5,30,5),
-          child:
-          TextField(
-            controller: nameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(
-                  width: 1.0,
-                ),
-              ),
-              hintText: 'Name'
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        Container(
-          padding: EdgeInsets.fromLTRB(30,5,30,5),
-          child:
-            TextField(controller: emailController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 1.0,
-                  ),
-                ),
-                hintText: 'Email',
-              ),
-            ),
-        ),
-        SizedBox(height: 10,),
-        Container(
-          padding: EdgeInsets.fromLTRB(30,5,30,5),
-          child:
-            TextField(controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 1.0,
-                  ),
-                ),
-                hintText: 'Password',
-              ),
-            ),
-        ),
-        SizedBox(height: 10,),
-        Container(
-          padding: EdgeInsets.fromLTRB(30,5,30,5),
-          child:
-            TextField(obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 1.0,
-                  ),
-                ),
-                hintText: 'Re-enter Password',
-              ),
-            ),
-        ),
-        SizedBox(height: 40,),
-        ElevatedButton(onPressed: registerUser,
-          style: ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll<Color>(Colors.black),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
-            padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(10),),
-            fixedSize: MaterialStateProperty.all<Size>(Size(200.0, 60.0),),
-          ),
-          child: Text('Register',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 28,
-            ),
-          ),
-        ),
-        SizedBox(height: 15,),
-        Row(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('I have an account. ', style: TextStyle(
-                fontSize: 16,
+            Text(
+              'Register',
+              style: TextStyle(
+                fontSize: 50,
+                fontFamily: 'Lilita One',
               ),
             ),
-            new InkWell(
-              child: new Text('Log in.', style: TextStyle(
-                  fontSize: 16,
-                color: Colors.blue,
+            SizedBox(height: 40),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 30, 5),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 1.0,
+                    ),
+                  ),
+                  hintText: 'Name',
                 ),
               ),
-              onTap: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => LoginScreen(),),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 30, 5),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 1.0,
+                    ),
+                  ),
+                  hintText: 'Email',
+                ),
               ),
-            ),],
-          ),],
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 30, 5),
+              child: TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 1.0,
+                    ),
+                  ),
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 30, 5),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 1.0,
+                    ),
+                  ),
+                  hintText: 'Re-enter Password',
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                registerUser();
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.black),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(10)),
+                fixedSize: MaterialStateProperty.all<Size>(Size(200.0, 60.0)),
+              ),
+              child: Text(
+                'Register',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'I have an account. ',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                InkWell(
+                  child: Text(
+                    'Log in.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
